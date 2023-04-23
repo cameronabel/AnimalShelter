b2d474ef5797c2fcd33c663389aa2f3359166657
Cameron Abel <cameronabel@gmail.com>
2023-04-23 04:53:23 -0700

    set up db connections

--- ShelterApi/Program.cs
+++ ShelterApi/Program.cs
@@ -1,8 +1,23 @@
+using ShelterApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<ShelterApiContext>(
+  dbContextOptions => dbContextOptions
+    .UseMySql(
+      builder.Configuration["ConnectionStrings:DefaultConnection"],
+      ServerVersion.AutoDetect(
+        builder.Configuration["ConnectionStrings:DefaultConnection"]
+      )
+    )
+);
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
